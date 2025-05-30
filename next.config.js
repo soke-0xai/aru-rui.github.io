6a91ab12178a530850d03df782e169c4f9c9ecea
soke <soke.0xai@outlook.jp>
2025-05-31 08:21:30 +0900

    Fix Next.js configuration for WebContainer

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    esmExternals: 'loose'
+    esmExternals: 'loose',
+    swcLoader: true,
+    forceSwcTransforms: true
   },
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
     return config;
-  },
-  swcMinify: false,
-  transpilePackages: ['@babel/preset-react']
+  }
 };
 
 module.exports = nextConfig;
